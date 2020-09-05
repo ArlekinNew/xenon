47460616842fa9d6d80eb3f8f7c0403e54a9b9c0
Evgeniy <novayapochta173@gmail.com>
2020-09-05 21:32:40 +0300

    'test'

--- routes/web.php
+++ routes/web.php
@@ -27,3 +27,15 @@ Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
 
+Route::group([
+	'prefix' => '/admin',
+	'namespace' => 'Admin',
+	'middleware' => ['auth']
+], function(){
+	Route::get('/', 'AdminController@index');
+
+	// Route::get('/service', 'ServiceController@index');
+	// Route::get('/order', 'OrderController@index');
+});
+
+
